0c00145d1e6cdae5a38f00f6a7b4247494e3fd95
Abbilion <frozzaryan@gmail.com>
2019-11-26 18:33:42 -0300

    jojofag

--- core/src/Menu/MenuPrincipal.java
+++ core/src/Menu/MenuPrincipal.java
@@ -14,7 +14,7 @@ public class MenuPrincipal implements Screen {
     public Botao jogar;
     private Botao opcoes;
     private Botao sair;
-    private Sprite paredeFundo, muralFundo, logo, calice;
+    private Sprite paredeFundo, muralFundo, logo, calice, calice2;
     private Sound musica;
 
     public MenuPrincipal(Egion game) {
@@ -22,10 +22,11 @@ public class MenuPrincipal implements Screen {
         paredeFundo = new Sprite(new Texture("botoes_img/parede.png"));
         muralFundo = new Sprite(new Texture("botoes_img/mural.png"));
         jogar = new Botao(100, 330, 200, 70, new Texture("botoes_img/jogarInativo.png"), new Texture("botoes_img/jogarAtivo.png"));
-        opcoes = new Botao(100, 245, 200, 70, new Texture("botoes_img/opcoes.png"), new Texture("botoes_img/botao1.jpg"));
-        sair = new Botao(100, 160, 200, 70, new Texture("botoes_img/sair.png"), new Texture("botoes_img/botao2.jpg"));
+        opcoes = new Botao(100, 245, 200, 70, new Texture("botoes_img/opcoes.png"), new Texture("botoes_img/opcoesAtivo.png"));
+        sair = new Botao(100, 160, 200, 70, new Texture("botoes_img/sair.png"), new Texture("botoes_img/sairAtivo.png"));
         logo = new Sprite(new Texture("botoes_img/jogarInativo.png"));
-        calice = new Sprite(new Texture("botoes_img/jogarInativo.png"));
+        calice = new Sprite(new Texture("botoes_img/folha.png"));
+        calice2 = new Sprite(new Texture("botoes_img/calice2.png"));
 
         paredeFundo.setSize(1080, 680);
         muralFundo.setSize(580, 480);
@@ -35,6 +36,8 @@ public class MenuPrincipal implements Screen {
         muralFundo.setCenter(340, 365);
         logo.setCenter(340, 490);
         calice.setCenter(460, 280);
+        calice2.setCenter(500, 255);
+        calice2.setSize(300, 180);
 
         musica = Gdx.audio.newSound(Gdx.files.internal("musica.mp3"));
         musica.play();
@@ -72,6 +75,7 @@ public class MenuPrincipal implements Screen {
         if(opcoes.verificaClick(game.batch)) {
             game.setScreen(new MenuConfig(game));
         }
+        calice2.draw(game.batch);
         game.batch.end();
     }
 
